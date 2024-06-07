ee9093b6dcd7acef7e4077c7218a2501ecb3b41f
Kalaivani <94457081+kalaivani007@users.noreply.github.com>
2024-06-07 15:41:38 +0530

    Update ActivationButton.php

--- ShipStream/Sync/Block/Adminhtml/System/Config/Fieldset/ActivationButton.php
+++ ShipStream/Sync/Block/Adminhtml/System/Config/Fieldset/ActivationButton.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace ShipStream\Sync\Block\Adminhtml\System\Config\Fieldset;
-
 use Magento\Config\Block\System\Config\Form\Field;
 use Magento\Framework\Data\Form\Element\AbstractElement;
-
 class ActivationButton extends Field
 {
     /**
@@ -13,14 +10,6 @@ class ActivationButton extends Field
      */
     protected function _getElementHtml(AbstractElement $element)
     {
-        $this->setElement($element);
-        $url = $this->getUrl('custommodule/controller/action'); // Specify the URL to the action controller
-        $html = $this->getLayout()->createBlock('Magento\Backend\Block\Widget\Button')
-            ->setType('button')
-            ->setClass('custom-button')
-            ->setLabel(__('Activate'))
-            ->setOnClick("setLocation('$url')")
-            ->toHtml();
-        return $html;
+    reutrn TRUE;
     }
 }
