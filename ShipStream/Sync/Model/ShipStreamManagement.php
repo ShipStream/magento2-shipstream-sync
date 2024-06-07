e236a1544e8d84936975d7041a44ec2343380252
Kalaivani <94457081+kalaivani007@users.noreply.github.com>
2024-06-07 17:28:54 +0530

    Update ShipStreamManagement.php

--- ShipStream/Sync/Model/ShipStreamManagement.php
+++ ShipStream/Sync/Model/ShipStreamManagement.php
@@ -4,6 +4,7 @@
  * See COPYING.txt for license details.
  */
 declare(strict_types=1);
+
 namespace ShipStream\Sync\Model;
 use Magento\Framework\App\ProductMetadataInterface;
 use Magento\Framework\Module\ModuleListInterface;
@@ -11,6 +12,7 @@ use Magento\Framework\App\Config\ScopeConfigInterface;
 use ShipStream\Sync\Helper\Data;
 use ShipStream\Sync\Model\Cron;
 use Psr\Log\LoggerInterface;
+
 class ShipStreamManagement implements \ShipStream\Sync\Api\ShipStreamManagementInterface
 {
 	protected $productMetadata;
