05a332e3a2ff08dfa61752aee8d39cc534b0c948
Kalaivani <94457081+kalaivani007@users.noreply.github.com>
2024-06-07 16:18:16 +0530

    Update ShipOrderInfo.php

--- ShipStream/Sync/Model/ShipOrderInfo.php
+++ ShipStream/Sync/Model/ShipOrderInfo.php
@@ -1,12 +1,9 @@
 <?php
-
 /**
  * Copyright ©  All rights reserved.
  * See COPYING.txt for license details.
  */
-
 declare(strict_types=1);
-
 namespace ShipStream\Sync\Model;
 
 use Magento\Sales\Api\OrderRepositoryInterface;
